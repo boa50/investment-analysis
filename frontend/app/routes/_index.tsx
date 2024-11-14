70a42b39e7f9e168a8d5b934cd8cc80e45b59cf4
boa50 <bruno.albuquerque50@gmail.com>
2024-11-14 11:47:12 -0300

    Created the structure of the frontend app

---
+++ frontend/app/routes/_index.tsx
@@ -0,0 +1,27 @@
+import type { MetaFunction } from "@remix-run/node";
+
+export const meta: MetaFunction = () => {
+  return [
+    { title: "Investment Analysis" },
+    { name: "description", content: "Index page" },
+  ];
+};
+
+export default function Index() {
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <div className="flex flex-col items-center gap-16">
+        <nav className="flex flex-col items-center justify-center gap-4 rounded-3xl border border-gray-200 p-6 dark:border-gray-700">
+          <p className="leading-6 text-gray-700 dark:text-gray-200">
+           Menu
+          </p>
+        </nav>
+        <header className="flex flex-col items-center gap-9">
+          <h1 className="leading text-2xl font-bold text-gray-800 dark:text-gray-100">
+            Investment Analysis
+          </h1>
+        </header>
+      </div>
+    </div>
+  );
+}
